a10ba7568f5fe7f7cb07e2c4a8576f0247a52028
therealhdcd <94260383+lolimhdcd@users.noreply.github.com>
2025-12-10 20:05:00 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/gif--ascii/",
   plugins: [react()],
+  base: './', // Use relative paths for GitHub Pages
   build: {
     outDir: 'dist',
     emptyOutDir: true,
   },
-});
+})
