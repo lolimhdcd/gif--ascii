a07e63b67d5e549286d1aa9b0c3edf77334c247b
therealhdcd <94260383+lolimhdcd@users.noreply.github.com>
2025-12-10 19:16:05 +0300

    Delete src/vite.config.ts

--- src/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// ⚠️ MUST MATCH THE HOMEPAGE PATH
-export default defineConfig({
-  base: "/gif--ascii/",
-  plugins: [react()]
-});
